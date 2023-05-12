dfe5520bf2dd41db4beff456b1abccaeba0adb13
linszay <lindseyjlancaster@gmail.com>
2023-05-11 23:35:43 -0500

    function uses .then and .catch

---
+++ ES6_promise/2-then.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+export default function handleResponseFromAPI(promise) {
+  promise
+    .then((data) => {
+      console.log('Got a response from the API');
+      return { status: 200, body: 'success' };
+    })
+    .catch((error) => {
+      console.log('Got an error from the API');
+      return new Error();
+    });
+}
