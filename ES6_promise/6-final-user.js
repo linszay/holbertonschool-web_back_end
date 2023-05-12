e1fdc3742b2275fb219dd2b9ec1c57f639379a6a
linszay <lindseyjlancaster@gmail.com>
2023-05-12 00:30:10 -0500

    modified file

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -3,18 +3,13 @@ import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
   try {
-    const [user, photo] = await Promise.all([
-      signUpUser(firstName, lastName),
-      uploadPhoto(fileName),
-    ]);
-
+    const user = await signUpUser(firstName, lastName);
+    const photo = await uploadPhoto(fileName);
     return [
       { status: 'fulfilled', value: user },
-      { status: 'fulfilled', value: photo },
+      { status: 'rejected', value: photo },
     ];
   } catch (error) {
-    return [
-      { status: 'rejected', reason: error },
-    ];
+    return [{ status: 'rejected', value: error.message }];
   }
 }
