4fe9d20b27165dc219f7849660c4597169464334
linszay <lindseyjlancaster@gmail.com>
2023-09-05 17:15:18 -0500

    modified documentation and added extra blank line

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
-#type-annotated function takes two floats and returns the sum as a float
+# type-annotated function takes two floats
+# returns the sum as a float
 def add(a: float, b: float) -> float:
     return (a + b)
+
