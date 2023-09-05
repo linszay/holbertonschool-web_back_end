283ae6336c00852ef54de8fb8a6c6274793ca64a
linszay <lindseyjlancaster@gmail.com>
2023-09-05 17:51:53 -0500

    modified type-annotation of function

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -2,6 +2,6 @@
 """type-annotated function floor which takes a float n as argument and returns the floor of the float"""
 
 
-def floor(n: float) -> float:
+def floor(n: float) -> int:
     """returns the floor of the float"""
     return (int(n // 1))
