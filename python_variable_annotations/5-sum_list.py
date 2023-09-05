e61d6baa6fbe630d04d3b95f7aa9db7203164e92
linszay <lindseyjlancaster@gmail.com>
2023-09-05 18:50:57 -0500

    added extra comment for comment check requirements

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -2,6 +2,7 @@
 import typing
 """type-annotated function sum_list which input_list of floats as arg"""
 """returns their sum as a float"""
+"""uses typing library for list type annotation"""
 
 
 def sum_list(input_list: typing.List[float]) -> float:
