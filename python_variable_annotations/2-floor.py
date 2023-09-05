de5175169e5b32be33839d38df17f8427c1f0555
linszay <lindseyjlancaster@gmail.com>
2023-09-05 17:54:14 -0500

    modified documentation to two lines for line length check

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-"""type-annotated function floor which takes a float n as argument and returns the floor of the float"""
+"""type-annotated function floor which takes a float n as argument"""
+"""returns the floor of the float"""
 
 
 def floor(n: float) -> int:
