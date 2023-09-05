609aa1d2cbab14ed1ae23963c152c0bcfd505a8c
linszay <lindseyjlancaster@gmail.com>
2023-09-05 17:29:35 -0500

    removed extra line at the end of file

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """type-annotated function takes two floats returns the sum as a float"""
+
+
 def add(a: float, b: float) -> float:
     """return sum as a float"""
     return (a + b)
-
