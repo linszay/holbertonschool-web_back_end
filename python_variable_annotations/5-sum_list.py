57ff25cd036b57e49d3fc7d92ec1e724757b7d45
linszay <lindseyjlancaster@gmail.com>
2023-09-05 18:54:55 -0500

    modified import placement to see if that was messing up my comments for checker

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-import typing
 """type-annotated function sum_list which input_list of floats as arg"""
 """returns their sum as a float"""
 """uses typing library for list type annotation"""
+import typing
 
 
 def sum_list(input_list: typing.List[float]) -> float:
