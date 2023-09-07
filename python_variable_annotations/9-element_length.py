d004c58597edfb521f5bf461c6a92d75f1a9b8a8
linszay <lindseyjlancaster@gmail.com>
2023-09-07 14:53:12 -0500

    pushing with different format for checker

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Annotate the below function’s parameters and return values with the appropriate types"""
+"""Annotate the function’s params and return values with appropriate types"""
 import typing
 
 
