0aa227d4f8b03015a71e74b3c3c8975be5b5f186
linszay <lindseyjlancaster@gmail.com>
2023-09-07 15:39:19 -0500

    added extra line needed and shortened ccomment

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
-"""takes in an integer argument (max_delay, with a default value of 10) & waits for a random delay between 0 and max_delay"""
+"""takes an arg max_delay, default value of 10 & waits for a random delay between 0 and max_delay"""
 import asyncio
 import random
+
 
 async def wait_random(max_delay: int = 10) -> float:
     """wait for random delay and returns it"""
