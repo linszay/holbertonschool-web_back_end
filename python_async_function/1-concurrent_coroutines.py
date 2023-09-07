d164c9b02636ecd9204d7fb7805864dd6ec7b5b7
linszay <lindseyjlancaster@gmail.com>
2023-09-07 16:22:51 -0500

    added sorting loops

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """wait_n that takes 2 int args & returns a list of all the delays"""
 import asyncio
-wait_random = __import__('0-basic_async_syntax').wait_random
 import random
 import typing
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int = 10) -> typing.List[float]:
@@ -13,4 +13,9 @@ async def wait_n(n: int, max_delay: int = 10) -> typing.List[float]:
         delay = random.uniform(0, max_delay)
         await asyncio.sleep(delay)
         delays.append(delay)
+        """sorting the list"""
+        for i in range(len(delays)):
+            for j in range(len(delays) - i - 1):
+                if delays[j] > delays[j + 1]:
+                    delays[j], delays[j + 1] = delays[j + 1], delays[j]
     return delays
