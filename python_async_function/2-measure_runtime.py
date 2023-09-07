9d1c489b4c58c8c36ae4cf9aafb5a8f8e88999b6
linszay <lindseyjlancaster@gmail.com>
2023-09-07 16:53:05 -0500

    added missing await

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -8,9 +8,7 @@ wait_n = __import__('1-concurrent_coroutines').wait_n
 async def measure_time(n: int, max_delay: int = 10) -> float:
     """returns total_time / n as a float"""
     start = time.time()
-    asyncio.run(wait_n(n, max_delay))
+    await asyncio.run(wait_n(n, max_delay))
     end = time.time()
     total = end - start
     return total / n
-
-
