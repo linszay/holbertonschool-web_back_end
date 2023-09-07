90c5fa9e5eeb453c60a610d55f12109ef096daf1
linszay <lindseyjlancaster@gmail.com>
2023-09-07 16:11:53 -0500

    wait_n that takes 2 int args & returns a list of all the delays

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""wait_n that takes 2 int args & returns a list of all the delays"""
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+import random
+import typing
+
+
+async def wait_n(n: int, max_delay: int = 10) -> typing.List[typing.Union[int, float]]:
+    """wait for random delay and returns it"""
+    delays = []
+    for _ in range(n):
+        delay = random.uniform(0, max_delay)
+        await asyncio.sleep(delay)
+        delays.append(delay)
+    return delays
