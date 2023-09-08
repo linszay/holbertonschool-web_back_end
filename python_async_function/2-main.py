9a061585f48166d07e4da8112cdab3cdafb72732
linszay <lindseyjlancaster@gmail.com>
2023-09-07 19:30:11 -0500

    still not getting correct output but repushing

--- python_async_function/2-main.py
+++ python_async_function/2-main.py
@@ -4,3 +4,5 @@ measure_time = __import__('2-measure_runtime').measure_time
 
 n = 5
 max_delay = 9
+
+print(measure_time(n, max_delay))
--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -8,7 +8,7 @@ wait_n = __import__('1-concurrent_coroutines').wait_n
 async def measure_time(n: int, max_delay: int) -> float:
     """returns total_time / n as a float"""
     start = time.time()
-    await asyncio.run(wait_n(n, max_delay))
+    asyncio.run(wait_n(n, max_delay))
     end = time.time()
     total = end - start
     return total / n
