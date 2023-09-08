75ab4bc591bb0e53dd2b09f8b53b4711b57dd034
linszay <lindseyjlancaster@gmail.com>
2023-09-07 19:12:51 -0500

    moved up variable placement

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """Annotate the function’s params and return values with appropriate types"""
 import typing
-
-
 seq = typing.Sequence
 tup = typing.Tuple
+
+
 def element_length(lst: typing.Iterable[seq]) -> typing.List[tup[seq, int]]:
     """annotate return with appropriate types"""
     return [(i, len(i)) for i in lst]
