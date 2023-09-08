82e4ccc4d5bd0bd9d08936a7377b7409e02615b5
linszay <lindseyjlancaster@gmail.com>
2023-09-07 19:02:32 -0500

    removed extra whitespace

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -9,9 +9,9 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 async def measure_runtime() -> float:
     """measures total runtime and returns it"""
     start = time.time()
-    await asyncio.gather(async_comprehension(), 
+    await asyncio.gather(async_comprehension(),
                          async_comprehension(), 
-                         async_comprehension(), 
+                         async_comprehension(),
                          async_comprehension())
     end = time.time()
     return end - start
