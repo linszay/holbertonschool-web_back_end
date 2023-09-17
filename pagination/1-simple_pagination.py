14395fb9a2ccc21e6c8a6f6797771170d4e0583a
linszay <lindseyjlancaster@gmail.com>
2023-09-17 17:16:04 -0500

    added remaining missing documentation

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -19,9 +19,11 @@ class Server:
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
+        """server constructor"""
         self.__dataset = None
 
     def dataset(self) -> List[List]:
+        """dataset"""
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
