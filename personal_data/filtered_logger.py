305ca302a3cc371c34e260e6c387a340a0becc62
linszay <lindseyjlancaster@gmail.com>
2023-09-20 14:30:19 -0500

    modified code to fix pattern error

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -4,11 +4,15 @@ import re
 from typing import List
 
 
-def filter_datum(fields: List[str], redaction: str, 
+def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     """use regex to replace occurences of certain field values"""
-    for field in fields:
-        """regex syntax to match current position, start of str, empty str"""
-        message = re.sub(fr'(?<=\{separator}|^){re.escape(field)}\b[^{separator}]*', redaction, message)
-
-    return message
+    """select separator char and its surrounding parentheses"""
+    """"|".join(map(re.escape, creates regex pattern to match fields and joins them"""
+    """capture separator character after field"""
+    """replace 1st capture group with 3rd capture group"""
+    return re.sub(
+        rf'({re.escape(separator)})({"|".join(map(re.
+        escape, fields))})({re.escape(separator)})', rf'\1{redaction}\3',
+        message
+    )
