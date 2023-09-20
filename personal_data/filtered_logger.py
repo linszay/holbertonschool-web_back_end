ba4604c2480e16fa99e64a943aa97717fe9f3419
linszay <lindseyjlancaster@gmail.com>
2023-09-20 15:07:38 -0500

    updated constructor and format to filter values in filter_datum

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -2,6 +2,28 @@
 """filter_datum returns log message obfuscated(type annotated)"""
 import re
 from typing import List
+import logging
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """updated to accept fields"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """filter values in incoming log records using filter_datum"""
+        message = super(RedactingFormatter, self).format(record)
+        for field in self.fields:
+            message = filter_datum([field], self.REDACTION, message, self.SEPARATOR)
+        return message
 
 
 def filter_datum(fields: List[str], redaction: str,
