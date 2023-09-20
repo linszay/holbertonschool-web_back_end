30d67cfcdf0d5237673d9b70691d48f635381760
linszay <lindseyjlancaster@gmail.com>
2023-09-20 17:09:59 -0500

    validates that the password matches the hashes password

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -2,8 +2,13 @@
 """takes string and returns salted, hashed password as byte string"""
 import bcrypt
 
+
 def hash_password(password: str) -> bytes:
     """returns password as byte string"""
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed_password
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """validates that the password matches the hashed password"""
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
