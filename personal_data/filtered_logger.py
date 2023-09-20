4b532a22556270851afec68c66b6554bbeabbca7
linszay <lindseyjlancaster@gmail.com>
2023-09-20 14:37:07 -0500

    trying to fix issue by adding a variable to make lines shorter

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -7,8 +7,12 @@ from typing import List
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     """use regex to replace occurences of certain field values"""
+    """select separator char and its surrounding parentheses"""
+    """"|".join(map(re.escape, creates regex pattern to match fields and joins them"""
+    """capture separator character after field"""
+    """replace 1st capture group with 3rd capture group"""
+    ree = re.escape
     return re.sub(
-        rf'({re.escape(separator)})({"|".join(map(re.
-        escape, fields))})({re.escape(separator)})', rf'\1{redaction}\3',
+        rf'({ree(separator)})({"|".join(map(ree, fields))})({ree(separator)})', rf'\1{redaction}\3',
         message
     )
