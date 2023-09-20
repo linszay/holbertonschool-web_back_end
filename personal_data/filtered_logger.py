d9ac249ed0b88fbae8b83f93de26301b136fb4c9
linszay <lindseyjlancaster@gmail.com>
2023-09-20 16:43:12 -0500

    added missing type annotation

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -55,7 +55,7 @@ def get_logger() -> logging.Logger:
         logger.addHandler(stream_handler)
         return logger
 
-def get_db():
+def get_db() -> mysql.connector.connection.MySQLConnection:
     """returns a connector to the database"""
     username = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
     password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
