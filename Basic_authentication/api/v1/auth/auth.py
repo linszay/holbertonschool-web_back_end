0c83bb41e4439cd357ffafd1cc679fcf379cd38c
linszay <lindseyjlancaster@gmail.com>
2023-09-20 21:09:54 -0500

    updated require auth with conditional statement

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -10,7 +10,18 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """returns false for now - path & excluded_paths used later"""
-        return False
+        if path is None or excluded_paths is None:
+            return True
+        elif len(excluded_paths) == 0:
+            return True
+        else:
+            for character in excluded_paths:
+                character = character.replace('/', '')
+                path = path.replace('/', '')
+                if path == character:
+                    return False
+        return True
+
 
     def authorization_header(self, request=None) -> str:
         """returns None - request will be Flask req object"""
