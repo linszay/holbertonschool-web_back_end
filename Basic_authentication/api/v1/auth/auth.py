7fe9c3e232993d25bd91596d9ca7ea28c8a26ecc
linszay <lindseyjlancaster@gmail.com>
2023-09-20 20:11:24 -0500

    removed blank lines for styling

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -8,16 +8,13 @@ from typing import List, TypeVar
 class Auth:
     """new class for auth management"""
 
-
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """returns false for now - path & excluded_paths used later"""
         return False
 
-
     def authorization_header(self, request=None) -> str:
         """returns None - request will be Flask req object"""
         return None
-
 
     def current_user(self, request=None) -> TypeVar('User'):
         """returns None - request will be Flask req object"""
