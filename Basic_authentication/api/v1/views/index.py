69f512d3ad8e1d558a33be29f8e9e2f01ad9f79f
linszay <lindseyjlancaster@gmail.com>
2023-09-20 19:37:23 -0500

    new error handler and endpoint;

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -13,6 +13,10 @@ def status() -> str:
     """
     return jsonify({"status": "OK"})
 
+@app_views.route('/unauthorized/', methods=['GET'], strict_slashes=False)
+def unauthorized() -> str:
+    """new endpoint raises 401 error"""
+    abort(401)
 
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
