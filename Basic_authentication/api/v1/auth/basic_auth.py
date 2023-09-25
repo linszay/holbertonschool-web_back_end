6a5eba4bb12027d3106c602f785ce8a2eb7f947a
linszay <lindseyjlancaster@gmail.com>
2023-09-25 15:29:13 -0500

    new method returns the decoded value of Base64 string

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """new class inherits from Auth"""
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -17,3 +18,17 @@ class BasicAuth(Auth):
             return None
         """split auth header after ' ' to only return Base64"""
         return authH.split(' ')[1]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str)\
+            -> str:
+        """returns the decoded base64 part of auth header"""
+        base = base64_authorization_header
+        if base is None or not isinstance(base, str):
+            return None
+        try:
+            decoded = base64.b64decode(base)
+            result = decoded.decode('utf-8')
+            return result
+        except Exception:
+            return None
