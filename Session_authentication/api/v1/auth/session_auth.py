5b6fcab48fe00abc51319537d8c258f7e577e3d7
linszay <lindseyjlancaster@gmail.com>
2023-09-26 17:43:33 -0500

    created new session_auth class and updated app.py to have new auth_type == session auth

---
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""new class inherits from Auth"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """new class inherits from Auth"""
+    def __init__(self):
+        pass
