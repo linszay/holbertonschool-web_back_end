d207d94a9a5f3bdf4d6f8fb56d86428ce2c5adf1
linszay <lindseyjlancaster@gmail.com>
2023-09-26 20:37:09 -0500

    method returns a cookie value from a request

--- Session_authentication/api/v1/auth/auth.py
+++ Session_authentication/api/v1/auth/auth.py
@@ -3,6 +3,7 @@
 import flask
 from flask import request
 from typing import List, TypeVar
+import os
 
 
 class Auth:
@@ -29,3 +30,13 @@ class Auth:
     def current_user(self, request=None) -> TypeVar('User'):
         """returns None - request will be Flask req object"""
         return None
+
+    def session_cookie(self, request=None):
+        """returns a cookie value from a request"""
+        if request is None:
+            return None
+        """get value of cookie my_session_id from env var session_name"""
+        sesssion = os.getenv('SESSION_NAME', '_my_session_id')
+        """using request to access the cookie from req cookie dict"""
+        cookie = request.cookies.get(sesssion)
+        return cookie
