3d61b4a7dd3e1415a5b0cf776fc1ccff95afdd59
linszay <lindseyjlancaster@gmail.com>
2023-09-26 19:46:18 -0500

    new class attribute and method that creates a session id for user_id

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -1,9 +1,23 @@
 #!/usr/bin/env python3
 """new class inherits from Auth"""
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """new class inherits from Auth"""
     def __init__(self):
         pass
+
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """method creates a session id for a user_id"""
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        """generates a universally unique identifier"""
+        session_id = str(uuid.uuid4())
+        """assigning value (session_id) to key (user_id)"""
+        SessionAuth.user_id_by_session_id[session_id] = user_id
+        return session_id
+
