67629ca6dd12dfb142ca30a0c2fb94d804745a37
linszay <lindseyjlancaster@gmail.com>
2023-09-27 01:07:44 -0500

    fixed line too long

--- Session_authentication/api/v1/views/session_auth.py
+++ Session_authentication/api/v1/views/session_auth.py
@@ -38,7 +38,8 @@ def login():
     return resp
 
 
-@app_views.route('/auth_session/logout', methods=['DELETE'], strict_slashes=False)
+@app_views.route('/auth_session/logout', methods=['DELETE'],
+                 strict_slashes=False)
 def auth_session_logout():
     """destroying the session"""
     from api.v1.app import auth
