514ca59e905d5e304a0921b49b52d3e3f31dc782
linszay <lindseyjlancaster@gmail.com>
2023-09-26 20:51:50 -0500

    updated method with new URL path and check for auth.session_cookie(request)

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -36,10 +36,12 @@ def before_request():
 
     """Paths that don't require authentication"""
     excluded_paths = ['/api/v1/status/', '/api/v1/status',
-                      '/api/v1/unauthorized/', '/api/v1/forbidden/']
+                      '/api/v1/unauthorized/', '/api/v1/forbidden/',
+                      '/api/v1/auth_session/login/']
 
     if request.path not in excluded_paths:
-        if auth.authorization_header(request) is None:
+        if auth.authorization_header(request) is None \
+            and auth.session_cookie(request) is None:
             abort(401)
 
         if auth.current_user(request) is None:
