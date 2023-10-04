66717576cbb5eaf773e239f6ebe9c2c7548a38dd
linszay <lindseyjlancaster@gmail.com>
2023-10-03 21:58:00 -0500

    creating a simple flask app and html template

---
+++ i18n/0-app.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""creating a simple flask app task 0"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    """rendering the index.html template"""
+    return render_template('index.html')
