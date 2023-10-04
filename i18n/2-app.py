79c280b1e4320c4d5dbe0bbe6add7a401579f933
linszay <lindseyjlancaster@gmail.com>
2023-10-04 14:37:10 -0500

    corrected function name and added documentation

--- i18n/2-app.py
+++ i18n/2-app.py
@@ -18,7 +18,8 @@ app.config.from_object(Config)
 
 
 @babel.localeselector
-def get_locales():
+def get_locale():
+    """request.accept_languages determines best match with our languages"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
