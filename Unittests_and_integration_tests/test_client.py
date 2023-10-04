eecfb1f2299a932199f820369e10b2cba3bd50ab
linszay <lindseyjlancaster@gmail.com>
2023-10-04 13:15:51 -0500

    mocking a property to test that it returns the expected result

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -3,7 +3,7 @@
 import unittest
 from parameterized import parameterized
 import unittest.mock
-from unittest.mock import patch, Mock
+from unittest.mock import patch, PropertyMock
 from client import GithubOrgClient
 
 
@@ -26,3 +26,14 @@ class TestGithubOrgClient(unittest.TestCase):
         """assert that get_json was only called once"""
         mock_get_json.assert_called_once_with(f"https://api.github.com/orgs/{orgss}")
 
+    @patch('client.GithubOrgClient.org', new_callable=PropertyMock)
+    def test_public_repos_url(self, mock_org):
+        """testing GithubOrgClient._public_repos_url"""
+        """using PropertyMock to mock a property"""
+        """setting expected return value"""
+        test_org_result = {'repos_url': 'https://api.github.com/orgs/test_orgs/repos'}
+        mock_org.return_value = test_org_result
+        """setting new_org_client to the mocked property"""
+        new_org_client = GithubOrgClient('test_repo')
+        """checking is result is equal"""
+        self.assertEqual(new_org_client._public_repos_url, 'https://api.github.com/orgs/test_orgs/repos')
