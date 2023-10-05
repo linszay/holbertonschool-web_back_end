7644ab3b042737304031ab70501e23e9fc1012c7
linszay <lindseyjlancaster@gmail.com>
2023-10-04 19:12:25 -0500

    more patching - added patch to mock get_sjon and used a patch as context manager to mock GithubOrgClient._public_repos_url

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -37,3 +37,28 @@ class TestGithubOrgClient(unittest.TestCase):
         new_org_client = GithubOrgClient('test_repo')
         """checking is result is equal"""
         self.assertEqual(new_org_client._public_repos_url, 'https://api.github.com/orgs/test_orgs/repos')
+
+    @patch('client.get_json')
+    @patch('client.GithubOrgClient._public_repos_url', new_callable=PropertyMock)
+    def test_public_repos(self, mock_public_repos_url, mock_get_json):
+        """Unit test for GithubOrgClient.public_repos"""
+        expected_payload = [
+            {"name": "repo1"},
+            {"name": "repo2"},
+        ]
+        """mock _public_repos_url property to return a URL"""
+        mock_public_repos_url.return_value = "https://api.github.com/orgs/test_org/repos"
+        """mock get_json to return the expected payload"""
+        mock_get_json.return_value = expected_payload
+        """creating an instance of GithubOrgClient"""
+        org_name = "test_org"
+        client = GithubOrgClient(org_name)
+        """calling public_repos method"""
+        repos = client.public_repos()
+        """assert that the mocked property was only called once"""
+        mock_public_repos_url.assert_called_once()
+        """assert that get_json was called once with the expected URL"""
+        mock_get_json.assert_called_once_with("https://api.github.com/orgs/test_org/repos")
+        """assert that result is what you expect for the payload"""
+        expected_repos = ["repo1", "repo2"]
+        self.assertEqual(repos, expected_repos)
