797a7ccdce64b11a4fea54d7bcd9732550e383f9
linszay <lindseyjlancaster@gmail.com>
2023-10-08 04:49:42 -0500

    added new methods get, get_str, and get_int

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -3,6 +3,7 @@
 import redis
 import uuid
 import typing
+from typing import Optional, Callable
 
 
 class Cache():
@@ -20,3 +21,23 @@ class Cache():
         self._redis.set(key, data)
         """return the key"""
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> typing.Union[str, bytes, int, float, None]:
+        """takes key and optional callable arg fn"""
+        """get data from regis"""
+        data = self._redis.get(key)
+        """if no data then return none"""
+        if data is None:
+            return None
+        """callable is used to convert the data to desired format"""
+        if fn is not None:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> typing.Union[str, None]:
+        """method automatically paramertizes cache.get to str"""
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: int) -> typing.Union[int, None]:
+        """method automatically paramertizes cache.get to int"""
+        return self.get(key, fn=int)
