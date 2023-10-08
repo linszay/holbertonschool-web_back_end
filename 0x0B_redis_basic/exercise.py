b388c1fc332f6d1cddddef11f3476111326e11f6
linszay <lindseyjlancaster@gmail.com>
2023-10-08 03:33:46 -0500

    updated typing for data parameter

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -2,16 +2,17 @@
 """Creating a new class & using Redis"""
 import redis
 import uuid
+import typing
 
 
 class Cache():
-    """class stores an instance od redis client as private variable"""
+    """class stores an instance of redis client as private variable"""
     def __init__(self):
         """Constructor"""
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: str) -> str:
+    def store(self, data: typing.Union[str, bytes, int, float]) -> str:
         """takes a data arg and returns a string"""
         """generate a randodm string"""
         key = str(uuid.uuid4())
