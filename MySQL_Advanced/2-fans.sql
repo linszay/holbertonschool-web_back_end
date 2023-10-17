371502a9d2feeea1c252e5c55e14c2ef35cbce58
linszay <lindseyjlancaster@gmail.com>
2023-10-17 18:12:56 -0500

    corrected total fan count to nb_fans

--- MySQL_Advanced/2-fans.sql
+++ MySQL_Advanced/2-fans.sql
@@ -1,6 +1,6 @@
 -- script ranks country origins of bands
 -- ordered by number of non-unique fans
-SELECT origin, SUM(fans) AS total_fans
+SELECT origin, SUM(fans) AS nb_fans
 FROM metal_bands
 GROUP BY origin
 ORDER BY total_fans DESC;
