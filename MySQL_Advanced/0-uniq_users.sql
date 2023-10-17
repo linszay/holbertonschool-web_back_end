1b4faa3412295341091e724368f2d968c4cf88cc
linszay <lindseyjlancaster@gmail.com>
2023-10-17 16:10:36 -0500

    created users database

---
+++ MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,7 @@
+-- creating users table
+-- has id, email, and name attributes
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
+)
