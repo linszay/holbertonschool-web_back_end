4f9989164ee58a9f6299beacc34df8841438808b
linszay <lindseyjlancaster@gmail.com>
2023-10-18 14:58:30 -0500

    fixed typo

--- MySQL_Advanced/11-need_meeting.sql
+++ MySQL_Advanced/11-need_meeting.sql
@@ -3,4 +3,4 @@
 -- or more than 1 month
 CREATE VIEW need_meeting AS SELECT name FROM students
 WHERE score < 80
-AND (last_meeting IS NULL OR last_meeting < DATE_SUB(CURBDATE(), INTERVAL 1 MONTH));
+AND (last_meeting IS NULL OR last_meeting < DATE_SUB(CURDATE(), INTERVAL 1 MONTH));
