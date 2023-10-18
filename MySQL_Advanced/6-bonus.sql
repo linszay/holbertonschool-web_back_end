ed6522f4455967c541ee25694416f4abbb09627c
linszay <lindseyjlancaster@gmail.com>
2023-10-18 12:51:46 -0500

    corrected procedure for if project id is null

--- MySQL_Advanced/6-bonus.sql
+++ MySQL_Advanced/6-bonus.sql
@@ -2,9 +2,14 @@
 -- it adds a new correction for a student
 DELIMITER //
 CREATE PROCEDURE AddBonus(IN student_id INT,
-IN project_name VARCHAR(100), IN score INT) BEGIN
+IN project_name VARCHAR(225), IN score INT) BEGIN
 DECLARE project_id INT;
-SET project_id = (SELECT id FROM projects WHERE name = project_name);
+SELECT id INTO project_id
+FROM projects WHERE name = project_name;
+IF project_id IS NULL THEN
+INSERT INTO projects (name) VALUES (project_name);
+SET project_id = LAST_INSERT_ID();
+END IF;
 INSERT INTO corrections (user_id, project_id, score) VALUES
 (student_id, project_id, score);
 END; 
