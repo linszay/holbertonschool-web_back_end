e36b2f713e78a704b76b03d7d520829abdcf505b
linszay <lindseyjlancaster@gmail.com>
2023-10-20 14:13:27 -0500

    fixed spacing for linter

--- Node_JS_basic/0-console.js
+++ Node_JS_basic/0-console.js
@@ -1,5 +1,5 @@
 // all funcs need to exported like module.exports = myFunc;
 // function displayMessage prints in STDOUT the string arg
 module.exports = function displayMessage(message) {
-    console.log(message);
-  };
+  console.log(message);
+};
