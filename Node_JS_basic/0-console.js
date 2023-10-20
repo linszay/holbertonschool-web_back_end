ddea0038aa8bd1c9d2b7ea5210d406fecfdc126e
linszay <lindseyjlancaster@gmail.com>
2023-10-20 13:58:02 -0500

    pushing inital files and func that prints in STDOUT the string arg

---
+++ Node_JS_basic/0-console.js
@@ -0,0 +1,5 @@
+// all funcs need to exported like module.exports = myFunc;
+// function displayMessage prints in STDOUT the string arg
+module.exports = function displayMessage(message) {
+    console.log(message);
+  };
