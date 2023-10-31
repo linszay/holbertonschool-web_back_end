4321962b8b84c2d522963bc8293376901d34aba8
linszay <lindseyjlancaster@gmail.com>
2023-10-31 14:39:52 -0500

    updated spacing for linter

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -3,16 +3,16 @@
 // when user exits, displays a message
 const readline = require('readline');
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-    });
+  input: process.stdin,
+  output: process.stdout
+  });
 
 console.log('Welcome to Holberton, what is your name?');
 
 rl.question('', (input) => {
-    console.log(`Your name is: ${input}`);
+  console.log(`Your name is: ${input}`);
 });
 
 rl.on('close', () => {
-    console.log('This important software is now closing');
+  console.log('This important software is now closing\n');
 });
