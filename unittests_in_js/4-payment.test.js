f11de07ccd2017f46d8b508031dc1e70059601fc
linszay <lindseyjlancaster@gmail.com>
2023-11-01 17:58:29 -0500

    corrected file with updated code

--- unittests_in_js/4-payment.test.js
+++ unittests_in_js/4-payment.test.js
@@ -1,21 +1,25 @@
-// copying code from task 3 for task 4
-// By using sinon.spy, make sure the math used for 
-// sendPaymentRequestToApi(100, 20) is the same as 
-// Utils.calculateNumber('SUM', 100, 20)
-// (validate the usage of the Utils function)
+// Stub Utils.calculateNumber to always return the same number 10
+// Verify the stub is called with type = SUM, a = 100, and b = 20
+// Add a spy to verify console.log is logging the correct message
+// The total is: 10
 const sinon = require('sinon');
-const sendPaymentRequestToApi = require('./3-payment');
+const sendPaymentRequestToApi = require('./4-payment');
 const Utils = require('./utils');
 
 describe('sendPaymentRequestToApi', () => {
-  it('should call Utils.calculateNumber with correct arguments', () => {
-    // creating a spooky spy for Utils.calculateNumber
-    const calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
+  it('should call Utils.calculateNumber with type SUM and display the correct message', () => {
+    // creating a stub for Utils.calculateNumber
+    const calculateNumberStub = sinon.stub(Utils, 'calculateNumber').returns(10);
+    // creating a spooky spy for console.log
+    const consoleLogSpy = sinon.spy(console, 'log');
     // calling the func
     sendPaymentRequestToApi(100, 20);
-    // verifing the spooky spy was called with the correct args
-    sinon.assert.calledWithExactly(calculateNumberSpy, 'SUM', 100, 20);
-    // restoring the spooky spy after use
-    calculateNumberSpy.restore();
+    // verifying the stub was called with correct args
+    sinon.assert.calledWithExactly(calculateNumberStub, 'SUM', 100, 20);
+    // verifying console.log is logging the correct message
+    sinon.assert.calledWithExactly(consoleLogSpy, 'The total is: 10');
+    // restore the stub and spy after use
+    calculateNumberStub.restore();
+    consoleLogSpy.restore();
   });
 });
