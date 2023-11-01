25d34a91032f3c41a88aa0124a15dc94c37badf3
linszay <lindseyjlancaster@gmail.com>
2023-10-31 20:00:01 -0500

    updated test file adn permissions

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -7,7 +7,7 @@ describe('calculateNumber', () => {
   it('should round two numbers and return their sum', () => {
     assert.strictEqual(calculateNumber(1, 3), 4);
     assert.strictEqual(calculateNumber(1, 3.7), 5);
-    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+    assert.strictEqual(calculateNumber(1.2, 3), 5);
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
   });
 });
