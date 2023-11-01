c49fc0f7856b5724c6cd226c42deca8f13c32a02
linszay <lindseyjlancaster@gmail.com>
2023-11-01 13:46:32 -0500

    reading a file async with node js

---
+++ Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,40 @@
+// new func takes a path arg
+// attempts to read the db file async
+// should return a promise
+const fs = require('fs');
+
+function countStudents(filePath) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'utf-8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+      } else {
+        const lines = data.split('\n');
+        const fieldCount = {};
+        const fieldNames = {};
+
+        for (const line of lines) {
+          const [firstName, field] = line.split(',');
+          if (firstName && field) {
+            if (!fieldCount[field]) {
+              fieldCount[field] = 1;
+              fieldNames[field] = [firstName];
+            } else {
+              fieldCount[field]++;
+              fieldNames[field].push(firstName);
+            }
+          }
+        }
+
+        console.log(`Number of students: ${lines.length - 1}`);
+        for (const field in fieldCount) {
+          console.log(`Number of students in ${field}: ${fieldCount[field]}. List: ${fieldNames[field].join(', ')}`);
+        }
+
+        resolve();
+      }
+    });
+  });
+}
+
+module.exports = countStudents;
