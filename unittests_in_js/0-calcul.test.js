b46cefcff7a06eb1f64fd10f3963fd5e40970bb1
linszay <lindseyjlancaster@gmail.com>
2023-11-01 17:00:08 -0500

    pushing agaaaain trying to get the checker to execute my files

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -7,8 +7,17 @@ const calculateNumber = require('./0-calcul');
 describe('calculateNumber', () => {
   it('should round two numbers and return their sum', () => {
     assert.strictEqual(calculateNumber(1.3, 0), 1);
+  });
+
+  it('should round two numbers and return their sum', () => {
     assert.strictEqual(calculateNumber(1, 3), 4);
+  });
+
+  it('should round two numbers and return their sum', () => {
     assert.strictEqual(calculateNumber(1, 3.7), 5);
+  });
+
+  it('should round two numbers and return their sum', () => {
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
   });
 });
