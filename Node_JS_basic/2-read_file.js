ae6cf88b08043f0a82e50c421be6b0bd5118e398
linszay <lindseyjlancaster@gmail.com>
2023-11-01 13:27:22 -0500

    updated code for linter

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -10,29 +10,31 @@ function countStudents(filePath) {
     const data = fs.readFileSync(filePath, 'utf-8');
     const lines = data.split('\n');
 
-    let totalStudents = 0;
     const fieldCount = {};
     const fieldNames = {};
 
     for (const line of lines) {
       const [firstName, field] = line.split(',');
       if (firstName && field) {
-        totalStudents++;
         if (!fieldCount[field]) {
           fieldCount[field] = 1;
           fieldNames[field] = [firstName];
         } else {
-          fieldCount[field]++;
+          fieldCount[field] += 1;
           fieldNames[field].push(firstName);
         }
       }
     }
 
-    console.log(`Number of students: ${totalStudents}`);
     for (const field in fieldCount) {
-      console.log(`Number of students in ${field}: ${fieldCount[field]}. List: ${fieldNames[field].join(', ')}`);
+      if (Object.prototype.hasOwnProperty.call(fieldCount, field)) {
+        console.log(`Number of students in ${field}: ${fieldCount[field]}.\
+         List: ${fieldNames[field].join(', ')}`);
+      }
     }
   } catch (error) {
     throw new Error('Cannot load the database');
   }
 }
+
+module.exports = countStudents;
