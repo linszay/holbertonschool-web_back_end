030f17e95683a3eae8bed6b5064d7c9a03c09663
linszay <lindseyjlancaster@gmail.com>
2023-11-01 21:53:27 -0500

    updated code again

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,19 +2,15 @@
 // user can input their name on new line
 // when user exits, displays a message
 
-const readline = require('readline');
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
 console.log('Welcome to Holberton School, what is your name?');
 
-rl.on('line', (input) => {
-  console.log(`Your name is: ${input}`);
+process.stdin.on('readable', () => {
+  const input = process.stdin.read();
+  if (input !== null) {
+    process.stdin.write(`Your name is: ${input}`);
+  }
 });
 
-rl.on('close', () => {
-  console.log('This important software is now closing');
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
