9326dac829f1668163531a62df64396e9fd15419
linszay <lindseyjlancaster@gmail.com>
2023-11-01 22:21:08 -0500

    changed stdin to stdout

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,12 +2,12 @@
 // user can input their name on new line
 // when user exits, displays a message
 
-process.stdout.write('Welcome to Holberton School, what is your name?');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('readable', () => {
   const name = process.stdin.read();
-  if (name !== null) {
-    process.stdin.write(`Your name is: ${name}`);
+  if (name) {
+    process.stdout.write(`Your name is: ${name}`);
   }
 });
 
