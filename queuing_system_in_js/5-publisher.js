70ba9649006f19a34a1dd2706cc8240dc767ff55
linszay <lindseyjlancaster@gmail.com>
2023-11-01 19:40:41 -0500

    node redis client publisher and subscriber

---
+++ queuing_system_in_js/5-publisher.js
@@ -0,0 +1,26 @@
+// Import the required library using ES6 import
+import redis from 'redis';
+
+// Create a Redis client instance
+const client = redis.createClient(6379, '127.0.0.1');
+
+// Handle Redis client connection events
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (error) => {
+  console.log(`Redis client not connected to the server: ${error}`);
+});
+
+const publishMessage = (message, time) => {
+  setTimeout(() => {
+    console.log(`About to send ${message}`);
+    client.publish('holberton school channel', message);
+  }, time);
+};
+  
+publishMessage('Holberton Student #1 starts course', 100);
+publishMessage('Holberton Student #2 starts course', 200);
+publishMessage('KILL_SERVER', 300);
+publishMessage('Holberton Student #3 starts course', 400);
---
+++ queuing_system_in_js/5-subscriber.js
@@ -0,0 +1,24 @@
+// Import the required library using ES6 import
+import redis from 'redis';
+
+// Create a Redis client instance
+const client = redis.createClient(6379, '127.0.0.1');
+
+// Handle Redis client connection events
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (error) => {
+  console.log(`Redis client not connected to the server: ${error}`);
+});
+  
+client.subscribe('holberton school channel');
+  
+client.on('message', (channel, message) => {
+  console.log(message);
+  if (message === 'KILL_SERVER') {
+    client.unsubscribe('holberton school channel');
+    client.quit();
+  }
+});
